b5115159f416137e34812b70edf03a6923049eec
Subham Gadtaula <sshoobham10@gmail.com>
2022-05-07 20:32:31 +0545

    first testing

--- VehicleRentingSystem/Owner/tests.py
+++ VehicleRentingSystem/Owner/tests.py
@@ -1,3 +1,13 @@
+from telnetlib import LOGOUT
 from django.test import TestCase
+from django.urls import *
 
-# Create your tests here.
+from VehicleRentingSystem.Owner.views import Profile
+
+def test_register_url(self):
+         url = reverse("Logout")
+         self.assertEquals(resolve(url).func, LOGOUT)
+
+def test_signin_url(self):
+         url = reverse("Profile")
+         self.assertEquals(resolve(url).func, Profile)
