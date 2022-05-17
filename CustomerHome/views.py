efce1b984e187fc578b0beffa03a34dae1563ca6
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-05-17 19:44:17 +0545

    branch pushed for use

--- CustomerHome/views.py
+++ CustomerHome/views.py
@@ -47,11 +47,80 @@ def index(request):
         return render(request,'index.html',{'Message':Message,'vehicle':vehicle})
     return render(request,'index.html',{'vehicle':vehicle})
 
+def signin(request):
+    return render(request,'SignIn.html')
 
-    return HttpResponse('Contact Us')
+def register(request):
+    return render(request,'register.html')
 
-def search(request):
-    query = request.GET['query']
-    vehicle = Vehicle.objects.filter(Vehicle_name__icontains=query)
-    params = {'vehicle': vehicle}
-    return render(request,'search_not_login.html', params)
+def LoginAuthentication(request):
+    global isLogin
+    login_email=request.POST.get('login_email','')
+    login_password=request.POST.get('login_password','')
+    # customer = Customer.objects.all()
+
+    result_customer = Customer.objects.filter(customer_email=login_email,customer_password=login_password)
+    result_owner = Owner.objects.filter(Owner_email=login_email,Owner_password=login_password)
+    result_manager = Manager.objects.filter(Manager_email=login_email,Manager_password=login_password)
+
+    if result_customer.exists():
+        request.session['user_email'] = login_email
+        isLogin = True
+        return redirect('/Home/')
+    elif result_owner.exists():
+        request.session['user_email'] = login_email
+        isLogin = True
+        return redirect('/Owner/')
+    elif result_manager.exists():
+        request.session['user_email'] = login_email
+        isLogin = True
+        return redirect('/Manager/')
+    else:
+        Message = "Invalid Email or password!!"
+        return render(request,'SignIn.html',{'Message':Message})
+
+def RegisterCustomer(request):
+    global isLogin
+
+    customer_firstname=request.POST.get('customer_firstname','')
+    customer_lastname=request.POST.get('customer_lastname','')
+    customer_dob=request.POST.get('customer_dob','')
+    customer_gender=request.POST.get('customer_gender','')
+    customer_mobileno=request.POST.get('customer_mobileno','')
+    customer_email=request.POST.get('customer_email','')
+    customer_password=request.POST.get('customer_password','')
+    customer_address=request.POST.get('customer_address','')
+    customer_city=request.POST.get('customer_city','')
+    customer_state=request.POST.get('customer_state','')
+    customer_country=request.POST.get('customer_country','')
+    customer_pincode=request.POST.get('customer_pincode','')
+    customer_license=request.FILES['customer_license']
+
+    result_customer = Customer.objects.filter(customer_email=customer_email)
+    result_owner = Owner.objects.filter(Owner_email=customer_email)
+    result_manager = Manager.objects.filter(Manager_email=customer_email)
+
+    if result_customer.exists() or result_owner.exists() or result_manager.exists():
+        Message = "This Email address already exists!!"
+        return render(request,'register.html',{'Message':Message})
+    else:
+        customer=Customer(customer_firstname=customer_firstname,customer_lastname=customer_lastname,
+        customer_dob=customer_dob,customer_gender=customer_gender,customer_mobileno=customer_mobileno,
+        customer_email=customer_email,customer_password=customer_password,customer_address=customer_address,
+        customer_city=customer_city,customer_state=customer_state,customer_country=customer_country,
+        customer_pincode=customer_pincode,customer_license=customer_license)
+        
+        customer.save()
+        request.session['user_email'] = customer_email
+        isLogin = True
+        return redirect('/Home/')
+
+def Logout(request):
+    global isLogout
+    del request.session['user_email']
+    isLogout = True
+    Message = "Successfully Logged Out!!"
+    return redirect('/')
+
+ Us')
+
