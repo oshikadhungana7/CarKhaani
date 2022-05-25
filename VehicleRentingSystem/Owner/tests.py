f21f4b857616bcc6d5a6df34b10b922f1ed98c54
Subham Gadtaula <sshoobham10@gmail.com>
2022-05-25 21:57:06 +0545

    testing

---
+++ VehicleRentingSystem/Owner/tests.py
@@ -0,0 +1,49 @@
+from telnetlib import LOGOUT
+from django.test import TestCase
+from django.urls import *
+from VehicleRentingSystem.CustomerHome.views import Logout
+
+from VehicleRentingSystem.Owner.views import Profile
+
+
+def test_register_url(self):
+    url = reverse("Logout")
+    self.assertEquals(resolve(url).func, Logout)
+
+
+def test_signin_url(self):
+    url = reverse("Profile")
+    self.assertEquals(resolve(url).func, Profile)
+
+
+def test_registercustomer_url(self):
+    url = reverse("RegisterCustomer")
+    self.assertEquals(resolve(url).func, RegisterCustomer)
+
+
+def test_loginauthen_url(self):
+    url = reverse("LoginAuthentication"),
+    self.assertEquals(resolve(url).func, LoginAuthentication)
+
+
+def test_home_url(self):
+    url = reverse("LoggedinHome"),
+    self.assertEquals(resolve(url).func, Home)
+
+
+def test_profile_url(self):
+    url = reverse("profile"),
+    self.assertEquals(resolve(url).func, Profile)
+
+
+def test_register_url(self):
+    url = reverse("Register")
+    self.assertEquals(resolve(url).func, register)
+
+def test_registercustomer_url(self):
+         url = reverse("RegisterCustomer")
+         self.assertEquals(resolve(url).func, RegisterCustomer)
+
+def test_loginauthen_url(self):
+         url = reverse("LoginAuthentication"),
+         self.assertEquals(resolve(url).func, LoginAuthentication)
